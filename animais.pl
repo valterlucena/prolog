882919e8175402f87d86cc847f9a5ced6e467d01
Valter Lucena <valter.lucena@ccc.ufcg.edu.br>
2018-02-08 14:54:59 -0300

    acho q ta certo to entendendo quase nada

--- animais.pl
+++ animais.pl
@@ -4,8 +4,6 @@ peixe(pedro).
 minhoca(maria).
 gato(miau).
 pessoa(eu).
-gosta(eu, miau).
-gosta(miau, eu).
 
 /* passaros gostam de minhocas */
 gosta(X,Y) :- passaro(X), minhoca(Y).
@@ -16,8 +14,13 @@ gosta(X,Y) :- gato(X), passaro(Y).
 /* gatos gostam de peixes */
 gosta(X,Y) :- gato(X), peixe(Y).
 
+/* gatos gostam de pessoas e vice-versa */
+gosta(X,Y) :- gato(X), pessoa(Y).
+gosta(X,Y) :- pessoa(X), gato(Y).
+
 /* amigos gostam uns dos outros */
-amigo(X,Y) :- gosta(X,Y) , gosta(Y,X).
+amigo(X,Y) :- gosta(X,Y), gosta(Y,X).
+
 
 /* gatos comem o que gostam, exceto pessoas */
-come(X,Y) :- gosta(gato, Y) , not(pessoa(Y)).
+come(X,Y) :- gosta(gato, Y), not(pessoa(Y)).
